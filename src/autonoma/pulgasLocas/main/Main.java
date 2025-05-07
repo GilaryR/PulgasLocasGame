91c65061b63e5b5787f54dd51f6687195ed77f7e
Alejandra Ortega <alejandrav.ortegad@autonoma.edu.co>
2025-05-07 13:08:05 -0500

    Se creo el paquete pulgasLocas.main con su respectivo main y PulgaGame

---
+++ src/autonoma/pulgasLocas/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pulgasLocas.main;
+
+import autonoma.pulgasLocas.main.PulgaGame;
+import autonoma.pulgasLocas.main.PulgaGame;
+
+/**
+ *
+ * @author Alejandra
+ */
+public class Main {
+    public static void main(String[] args) {
+        PulgaGame juego = new PulgaGame();
+    }
+}
---
+++ src/autonoma/pulgasLocas/main/PulgaGame.java
@@ -0,0 +1,93 @@
+package autonoma.pulgasLocas.main;
+
+import autonoma.pulgasLocas.elements.CampoBatalla;
+import autonoma.pulgasLocas.elements.SpawnPulga;
+import autonoma.pulgasLocas.ui.PulgasLocasWindow;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Alejandra
+ */
+public class PulgaGame implements Runnable {
+
+    private Thread gameLoopThread;
+    private PulgasLocasWindow ventana;
+    private CampoBatalla campo;
+    private SpawnPulga spawner;
+    private final int FPS_SET = 5;
+    private final int UPS_SET = 20;
+    public PulgaGame() {
+        this.ventana = new PulgasLocasWindow();
+        this.campo = new CampoBatalla(ventana, this);
+        ventana.setCampoBatalla(campo);
+        initClasses();
+        this.ventana.requestFocus();
+        this.ventana.setVisible(true);
+        this.startGameLoop();
+    }
+    public void update() {
+        this.campo.update();
+    }
+    public void render(Graphics g) {
+        this.campo.paint(g);
+    }
+
+    private void startGameLoop() {
+        gameLoopThread = new Thread(this);
+        gameLoopThread.start();
+    }
+
+    private void initClasses() {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                campo.agregarPulgaNormal();
+            } else {
+                campo.agregarPulgaMutante();
+            }
+        }
+        this.spawner = new SpawnPulga(campo);
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+        long previousTime = System.nanoTime();
+        int frames = 0;
+        int updates = 0;
+        double deltaU = 0;
+        double deltaF = 0;
+        long lastCheck = System.currentTimeMillis();
+       
+        while (true) {
+            long currentTime = System.nanoTime();
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+            if (deltaU >= 1) {
+                update();
+                updates++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {
+                this.ventana.repaint();
+                deltaF--;
+                frames++;
+            }
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames + "| UPS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+    }
+
+    public SpawnPulga getSpawner() {
+        return spawner;
+    }
+
+   
+}
