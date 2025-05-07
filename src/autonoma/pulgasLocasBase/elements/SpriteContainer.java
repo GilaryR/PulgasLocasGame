ba33598726bff3e5caac4bd33e2802bac1e2662c
Luisa Fernanda Henao Posada <tuemail@example.com>
2025-05-07 12:41:05 -0500

    Se agrega la documentacion Java.doc para SpriteMobile

--- src/autonoma/pulgasLocasBase/elements/SpriteContainer.java
+++ src/autonoma/pulgasLocasBase/elements/SpriteContainer.java
@@ -1,38 +1,71 @@
-
 package autonoma.pulgasLocasBase.elements;
 import java.util.ArrayList;
 
 /**
- *
- * @author educacion
+ * Clase abstracta que representa un contenedor de sprites. Extiende {@link Sprite}
+ * e implementa la interfaz {@link GraphicContainer}.
+ * 
+ * <p>Esta clase permite contener y gestionar múltiples sprites, facilitando operaciones
+ * como agregar, eliminar y contar objetos gráficos.</p>
+ * 
+ * @author Luisa Fernanda Henao Posada
+ * @since 2025-05-05
+ * @version 1.0.0
  */
-public abstract class SpriteContainer extends Sprite implements GraphicContainer
-{
-    protected ArrayList<Sprite> sprites;   
-    
+public abstract class SpriteContainer extends Sprite implements GraphicContainer {
+
+    /**
+     * Lista que contiene todos los sprites dentro del contenedor.
+     */
+    protected ArrayList<Sprite> sprites;
+
+    /**
+     * Constructor que inicializa la posición y tamaño del contenedor, además de la lista de sprites.
+     *
+     * @param x Coordenada horizontal del contenedor.
+     * @param y Coordenada vertical del contenedor.
+     * @param height Altura del contenedor.
+     * @param width Anchura del contenedor.
+     */
     public SpriteContainer(int x, int y, int height, int width) {
         super(x, y, height, width);
-        
-        sprites = new ArrayList<Sprite>();
-    }   
-    
-    public boolean add(Sprite sprite)
-    {
+        sprites = new ArrayList<>();
+    }
+
+    /**
+     * Agrega un sprite a la lista del contenedor.
+     * 
+     * @param sprite Objeto {@link Sprite} a agregar.
+     * @return {@code true} si fue agregado exitosamente.
+     */
+    public boolean add(Sprite sprite) {
         return sprites.add(sprite);
     }
-    
-    public void remove(int index)
-    {
+
+    /**
+     * Elimina el sprite ubicado en la posición indicada.
+     * 
+     * @param index Índice del sprite en la lista.
+     */
+    public void remove(int index) {
         sprites.remove(index);
     }
 
-    public void remove(Sprite sprite)
-    {
+    /**
+     * Elimina un sprite específico de la lista.
+     * 
+     * @param sprite Objeto {@link Sprite} que se desea eliminar.
+     */
+    public void remove(Sprite sprite) {
         sprites.remove(sprite);
     }
-    
-    public int size()
-    {
+
+    /**
+     * Retorna la cantidad de sprites almacenados en el contenedor.
+     * 
+     * @return Número total de sprites.
+     */
+    public int size() {
         return sprites.size();
     }
 }
--- src/autonoma/pulgasLocasBase/elements/SpriteMobile.java
+++ src/autonoma/pulgasLocasBase/elements/SpriteMobile.java
@@ -1,23 +1,50 @@
-
 package autonoma.pulgasLocasBase.elements;
-import autonoma.pulgasLocasBase.elements.Sprite;
 
 /**
- *
- * @author educacion
+ * Clase abstracta que representa un sprite con capacidad de movimiento.
+ * Extiende la clase {@link Sprite} añadiendo un atributo de desplazamiento.
+ * 
+ * <p>Esta clase base es útil para definir sprites animados o móviles como jugadores,
+ * enemigos o elementos que cambian de posición.</p>
+ * 
+ * @author Luisa Fernanda Henao Posada
+ * @since 2025-05-05
+ * @version 1.0.0
+ * 
  */
-public abstract class SpriteMobile extends Sprite
-{
+public abstract class SpriteMobile extends Sprite {
+
+    /**
+     * Cantidad de píxeles que el sprite se moverá por paso.
+     */
     protected int step;
-    
+
+    /**
+     * Constructor que inicializa la posición y tamaño del sprite móvil.
+     *
+     * @param x Coordenada horizontal inicial.
+     * @param y Coordenada vertical inicial.
+     * @param height Altura del sprite.
+     * @param width Anchura del sprite.
+     */
     public SpriteMobile(int x, int y, int height, int width) {
         super(x, y, height, width);
     }
 
+    /**
+     * Obtiene el valor de desplazamiento por paso.
+     * 
+     * @return Cantidad de píxeles por paso.
+     */
     public int getStep() {
         return step;
     }
 
+    /**
+     * Establece el valor de desplazamiento por paso.
+     * 
+     * @param step Cantidad de píxeles por paso.
+     */
     public void setStep(int step) {
         this.step = step;
     }
